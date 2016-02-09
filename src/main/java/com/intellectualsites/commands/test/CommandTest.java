02f4f440659878a71d95785b39926797d9570235
Sauilitired <citymonstret@hotmail.com>
2016-02-09 19:41:18 +0100

    Make the examples easier to understand

--- src/main/java/com/intellectualsites/commands/test/CommandTest.java
+++ src/main/java/com/intellectualsites/commands/test/CommandTest.java
@@ -28,11 +28,16 @@ public class CommandTest {
     }
 
     public static void main(String[] args) {
+        //
+        // Create a command caller
+        //
         CommandCaller caller = new SystemCaller();
+
+        //
+        // Let's create a manager
+        //
         final CommandManager manager = new CommandManager();
-        if(!manager.createCommand(new TestCommand())) {
-            System.out.println("Failed to create command :(");
-        }
+        // Set some (optional) settings
         manager.getManagerOptions().setRequirePrefix(false).setUseAdvancedPermissions(true)
                 .setPermissionChecker(new AdvancedPermission.PermissionChecker() {
                     public boolean isPermitted(CommandCaller caller, Command command) {
@@ -42,37 +47,25 @@ public class CommandTest {
                         return caller.hasAttachment(command.getPermission());
                     }
                 });
+        // Create a command from annotation (see the command below)
+        if(!manager.createCommand(new TestCommand())) {
+            System.out.println("Failed to create command :(");
+        }
+        // Let's test the "get closets match" system
         debug(caller, manager.handle(caller, "tlest banana 10"));
+        // Let's try to call it
         debug(caller, manager.handle(caller, "test banana 10"));
 
+        //
+        // Test the pagination command type
+        //
         final List<Dog> dogList = new ArrayList<Dog>();
         for (int i = 0; i < 50; i++) {
             dogList.add(new Dog(i + ""));
         }
-
-        PaginatedCommand<Dog> paginatedCommand = new PaginatedCommand<Dog>(
-                Dog.class, dogList, 10, "dogs", "/dogs [page]", "List dogs", "dogs.permission", new String[0], Object.class
-        ) {
-            {
-                withArgument("page", ArgumentType.Integer, "Dog page");
-            }
-
-            @Override
-            public boolean handleTooBigPage(CommandInstance instance, int specifiedPage, int maxPages) {
-                instance.getCaller().message("Page too big (" + specifiedPage + " < " + maxPages + ")");
-                return true;
-            }
-
-            @Override
-            public boolean onCommand(PaginatedCommandInstance instance) {
-                for (int i = 0; i < instance.getPage().getItems().length; i++) {
-                    instance.getCaller().message("[" + (i+1) + "] " + (Dog) instance.getPage().getItems()[i]);
-                }
-                return true;
-            }
-        };
-
-        manager.addCommand(paginatedCommand);
+        // Add it
+        manager.addCommand(new PaginationTest(dogList));
+        // Call it
         debug(caller, manager.handle(caller, "dogs 5"));
 
         // for (CommandManager command : CommandCloud.get('/')) {
@@ -82,6 +75,28 @@ public class CommandTest {
         //        caller.message("Found manager: " + manager.getMeta("environment"));
         //    }
         // }
+    }
+
+    public static class PaginationTest extends PaginatedCommand<Dog> {
+
+        public PaginationTest(List<Dog> items) {
+            super(Dog.class, items, 10, "dogs", "/dogs [page]", "List dogs", "dogs.permission", new String[0], Object.class);
+            withArgument("page", ArgumentType.Integer, "Dog page");
+        }
+
+        @Override
+        public boolean handleTooBigPage(CommandInstance instance, int specifiedPage, int maxPages) {
+            instance.getCaller().message("Page too big (" + specifiedPage + " < " + maxPages + ")");
+            return true;
+        }
+
+        @Override
+        public boolean onCommand(PaginatedCommandInstance instance) {
+            for (int i = 0; i < instance.getPage().getItems().length; i++) {
+                instance.getCaller().message("[" + (i+1) + "] " + instance.getPage().getItems()[i]);
+            }
+            return true;
+        }
     }
 
     @CommandDeclaration(command = "test", usage = "/test [word]")
